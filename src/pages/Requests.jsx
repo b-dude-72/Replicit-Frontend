0bccb70287b772d814bb9c632a6acb6787228a6e
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-03-11 22:15:02 +0530

    add show all docs and show respective r docs

--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -7,13 +7,18 @@ const Requests = () => {
     let navigate = useNavigate();
     let localAuth = localStorage.getItem('auth-token')
     const [alldrs, setAllDrs] = useState([]);
+    const [myDrs, setMyDrs] = useState([]);
+    const [show, setShow] = useState(false);
+    /*
+    show = false -> show all docs
+    show = true -> show my docs
+    */
 
-    const [page,setPage]= useState(1);
-    const [pageCount,setPageCount]= useState(0);
+    const [page, setPage] = useState(1);
+    const [pageCount, setPageCount] = useState(0);
 
 
     const getAllDocs = () => {
-
         const requestOptions = {
             method: 'GET',
             headers: {
@@ -30,6 +35,22 @@ const Requests = () => {
             );
     }
 
+    const getMyDocs = () => {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': localAuth
+            },
+        };
+        fetch(`${process.env.REACT_APP_URL}/api/auth/getdrmr`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                setMyDrs(data.items);
+            }
+            );
+    }
+
     console.log(alldrs)
     console.log(pageCount + " pageCount")
 
@@ -38,27 +59,30 @@ const Requests = () => {
             navigate('/login')
         }
         getAllDocs()
+        getMyDocs();
     }, [page])
 
 
-    function handlePrevious(){
-        setPage((p)=>{
-            if(p===1){
+    function handlePrevious() {
+        setPage((p) => {
+            if (p === 1) {
                 return 1;
             }
             return p - 1;
         })
     }
 
-    function handleNext(){
-        setPage((p)=>{
+    function handleNext() {
+        setPage((p) => {
             // if(p===pageCount){
-            if(p===Math.ceil(pageCount)){
+            if (p === Math.ceil(pageCount)) {
                 return p;
             }
             return p + 1;
         })
     }
+
+    console.log(myDrs)
 
     return (
         <>
@@ -66,39 +90,73 @@ const Requests = () => {
             <Sidebar />
             <div className="">
 
-
-            <div className='flex flex-col w-full'>
-                <div className='w-full'>
-                    <h2 className='text-black text-center bg-slate-300 capitalize font-serif h-10 items-center justify-center flex  '>
-                        All the requests
-                    </h2>
-                    <table className='mx-5 mt-5'>
-                        <thead class="bg-white border-2">
-                            <tr>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                    Name
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                    Email
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                    Status
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <Alldrs drs={alldrs} />
-                        </tbody>
-                    </table>
+                <div className='flex flex-col w-full'>
+                    <div className='w-full'>
+                        <h2 className='text-black text-center bg-slate-300 capitalize font-serif h-10 items-center justify-center flex  '>
+                            All the requests
+                        </h2>
+                        <section className='flex flex-row w-full justify-around bg-red-500'>
+                            <p className='cursor-pointer bg-yellow-200' onClick={()=>{setShow(false)}} >All Requests</p>
+                            <p className='cursor-pointer bg-yellow-200' onClick={()=>{setShow(true)}} >Your Requests</p>
+                        </section>
+                        {show ==false &&
+                        <div>
+                            <p>All Requests</p>
+                            <table className='mx-5 mt-5'>
+                            <thead class="bg-white border-2">
+                                <tr>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Email
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Status
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <Alldrs drs={alldrs} />
+                            </tbody>
+                        </table>
+                        </div>
+                        }
+                        {show==true &&
+                        <div>
+                            <p>Your requests Requests</p>
+                            <table className='mx-5 mt-5'>
+                            <thead class="bg-white border-2">
+                                <tr>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Email
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Status
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <Alldrs drs={myDrs} />
+                            </tbody>
+                        </table>
+                        </div>
+                        }
+                    </div>
                 </div>
-            </div>
-        <div className="page_control flex py-2 w-full bg-[#4fbae7] flex-row justify-around">
-                <button className='text-sm text-white px-7 hover:bg-blue-700 py-2 border-red bg-blue-800 border-1 rounded-xl font-bold uppercase border-black cursor-pointer' disabled={page==1} onClick={handlePrevious}>Previous</button>
-                <button className='text-sm text-white px-7 hover:bg-blue-700 py-2 border-red bg-blue-800 border-1 rounded-xl font-bold uppercase border-black cursor-pointer' disabled={page == pageCount } onClick={handleNext}>Next</button>
-                {/* <b>pagge: {page}</b>
+                {show==false &&
+
+                    <div className="page_control flex py-2 w-full bg-[#4fbae7] flex-row justify-around">
+                    <button className='text-sm text-white px-7 hover:bg-blue-700 py-2 border-red bg-blue-800 border-1 rounded-xl font-bold uppercase border-black cursor-pointer' disabled={page == 1} onClick={handlePrevious}>Previous</button>
+                    <button className='text-sm text-white px-7 hover:bg-blue-700 py-2 border-red bg-blue-800 border-1 rounded-xl font-bold uppercase border-black cursor-pointer' disabled={page == pageCount} onClick={handleNext}>Next</button>
+                    {/* <b>pagge: {page}</b>
                 <b>page count: {pageCount}</b> */}
-            </div>      
-</div>
+                </div>
+                }
+            </div>
         </>
     );
 };
