1004d8761a1bec522dfbc1b088b17eae785164f8
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-03-14 12:42:40 +0530

    done forgot password

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -39,7 +39,18 @@ const ForgotPassword = () => {
                 email: "",
             })
         }
-
+        if(json.message){
+            setError(json.message)
+            setCred({
+                email:""
+            })
+        }
+        setTimeout(() => {
+            setCred({
+                email: "",
+            })
+            setError("")
+        }, 1000);        
     }
 
     return (
@@ -61,7 +72,7 @@ const ForgotPassword = () => {
                                         <div class="relative">
                                             <input autocomplete="off" onChange={onChange
                                             } id="email" name="email" type="email" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 mb-2" placeholder="Email address" />
-                                            <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+                                            <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm" value={cred.email}>Email Address</label>
                                         </div>
                                         <div class="relative flex flex-rwo justify-between w-full">
                                             <button onClick={submitForm} class="bg-blue-700 hover:bg-blue-600 hover:font-semibold hover:easein text-white rounded-md px-4 py-1 mt-2">Send Email</button>
