eafb881f469f10d737ebd694c83ca1f1a8d75e62
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-03-15 18:27:20 +0530

    stats

---
+++ src/components/DashboardStats.jsx
@@ -0,0 +1,161 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+
+
+const DashBoardStats = () => {
+    let localAuth = localStorage.getItem('auth-token')
+    const [currUser, setCurrUser] = useState();
+    const [allDrs, setAllDrs] = useState();
+
+    const getCurrentUserData = () => {
+
+
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': localAuth,
+            },
+        };
+        fetch(`${process.env.REACT_APP_URL}/api/auth/getmemberdetails`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+
+                setCurrUser(data)
+            }
+            );
+    }
+
+    useEffect(() => {
+        getCurrentUserData()
+        getAllDocs()
+    }, [])
+
+    // console.log("curr user role:" + currentUserRole)
+
+    const [countNew, setCountNew] = useState();
+    const [countRejected, setCountRejected] = useState();
+    const [countMr, setCountMr] = useState();
+    const [countDone, setCountDone] = useState();
+
+    //  total number of pending  requests -> all new 
+    //  total number of rejected  requests -> all rejected 
+    //  total number of MRs -> mr count
+    //  total number of live websites -> done
+
+    const getAllDocs = () => {
+
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': localAuth
+            },
+        };
+        fetch(`${process.env.REACT_APP_URL}/api/auth/getdrscount`, requestOptions)
+            .then(response => response.json())
+            .then(data => setAllDrs(data.drs));
+    }
+
+
+    // let done;
+    // let reject;
+    // let newdr;
+    let currentUserRole;
+
+    if (currUser) {
+    //     // console.log(currUser)
+        currentUserRole = currUser.user.role;
+    //     done = allDrs.filter(
+    //         function (ele) {
+    //             return ele.status == 'Done';
+    //         }
+    //     ).length;
+    //     newdr = allDrs.filter(
+    //         function (ele) {
+    //             return ele.status == 'New';
+    //         }
+    //     ).length;
+    //     reject = allDrs.filter(
+    //         function (ele) {
+    //             return ele.status == "Rejected";
+    //         }
+    //     ).length;
+
+    }
+
+    // console.log("Done: " + done)
+    // console.log(allDrs)
+    // console.log(typeof(allDrs))
+
+    // if(newdr){     
+    //     setCountNew(newdr);
+    //     setCountRejected(reject);
+    //     setCountDone(done);
+    // }
+
+    return (
+        <>
+            {
+
+                currentUserRole == 3 &&
+                <div class="flex items-center  bg-gray-200 text-gray-800" >
+                    <div class="p-4 w-full">
+                        <div class="grid grid-cols-12 gap-4">
+                            <div class="col-span-12 sm:col-span-6 md:col-span-3">
+                                <div class="flex flex-row bg-white shadow-sm rounded p-4">
+                                    <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-blue-100 text-blue-500">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                                    </div>
+                                    <div class="flex flex-col flex-grow ml-4">
+                                        <div class="text-sm text-gray-500">New Requests</div>
+                                        <div class="font-bold text-lg">27</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-span-12 sm:col-span-6 md:col-span-3">
+                                <div class="flex flex-row bg-white shadow-sm rounded p-4">
+                                    <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-green-100 text-green-500">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
+                                    </div>
+                                    <div class="flex flex-col flex-grow ml-4">
+                                        <div class="text-sm text-gray-500">Done Requests</div>
+                                        <div class="font-bold text-lg">4</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-span-12 sm:col-span-6 md:col-span-3">
+                                <div class="flex flex-row bg-white shadow-sm rounded p-4">
+                                    <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-orange-100 text-orange-500">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
+                                    </div>
+                                    <div class="flex flex-col flex-grow ml-4">
+                                        <div class="text-sm text-gray-500">Rejected Requests</div>
+                                        <div class="font-bold text-lg">16</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-span-12 sm:col-span-6 md:col-span-3">
+                                <div class="flex flex-row bg-white shadow-sm rounded p-4">
+                                    <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl bg-red-100 text-red-500">
+                                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    </div>
+                                    <div class="flex flex-col flex-grow ml-4">
+                                        <div class="text-sm text-gray-500">Total MR</div>
+                                        <div class="font-bold text-lg">23</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div >
+
+
+            }
+        </>
+
+    )
+}
+
+
+export default DashBoardStats
