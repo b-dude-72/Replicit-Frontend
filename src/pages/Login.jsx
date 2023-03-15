4acfa5fe02b42dc1e9f917d47da9f06b54c041cf
Abhishek Pathak <abhishekpathak1720@gmail.com>
2023-03-15 17:09:17 +0530

    add eye symbol

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import {
     Link,
-  } from "react-router-dom";
+} from "react-router-dom";
 
-
+import { FaEye } from "react-icons/fa";
 const Login = () => {
 
     // let URL = `http://localhost:5000`
@@ -50,9 +50,21 @@ const Login = () => {
 
     }
 
-    useEffect(()=>{
-        document.title="Login - Replicit"
+    useEffect(() => {
+        document.title = "Login - Replicit"
     })
+
+    const [type,setType] = useState("password");
+
+    const passwordchange = () =>{
+        // let input = document.getElementById("password");
+        if(type =="password"){
+            setType("text")
+        }else{
+            setType("password")
+        }
+
+    }
 
     return (
         <div class="w-[100%]">
@@ -74,9 +86,15 @@ const Login = () => {
                                         <label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
                                     </div>
                                     <div class="relative">
-                                        <input value={cred.password} autocomplete="off" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 " placeholder="Password" onChange={onChange
+                                        <div className="flex flex-row justify-between items-center">
 
-                                        } />
+                                            <input value={cred.password} autocomplete="off" id="password" name="password" type={type} class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 " placeholder="Password" onChange={onChange
+
+                                            } />
+                                            <div className="text-xl mb-2 cursor-pointer" onClick={passwordchange}>
+                                                <FaEye />
+                                            </div>
+                                        </div>
                                         <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm ">Password</label>
                                     </div>
                                     <div class="relative flex flex-rwo justify-between w-full">
